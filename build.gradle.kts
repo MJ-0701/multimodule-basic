92a21ffd2c33a6b003e3c7264019c2bca7851f61
MJ-0701 <jack2718@naver.com>
2023-06-15 01:50:23 +0900

    그레이들 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.ir.backend.js.compile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "3.1.0" apply false
     id("io.spring.dependency-management") version "1.1.0" apply false
     kotlin("jvm") version "1.8.21"
+    kotlin("kapt") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21" apply false
     kotlin("plugin.spring") version "1.8.21" apply false
-    kotlin("kapt") version "1.8.21"
+    kotlin("plugin.allopen") version "1.8.21" apply false
 
 }
 
@@ -25,13 +27,13 @@ allprojects{
 
 subprojects{
     apply{
+        plugin("io.spring.dependency-management")
+        plugin("org.springframework.boot")
         plugin("org.jetbrains.kotlin.jvm")
         plugin("org.jetbrains.kotlin.kapt")
-        plugin("org.jetbrains.kotlin.plugin.spring")
         plugin("org.jetbrains.kotlin.plugin.jpa")
-        plugin("org.springframework.boot")
-        plugin("io.spring.dependency-management")
-        plugin("kotlin-spring")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("org.jetbrains.kotlin.plugin.allopen")
     }
 
     dependencies {
@@ -42,6 +44,8 @@ subprojects{
         implementation("org.springframework.boot:spring-boot-devtools")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.springframework.security:spring-security-test")
+
+
     }
 
     tasks.withType<KotlinCompile> {
@@ -49,6 +53,11 @@ subprojects{
             freeCompilerArgs = listOf("-Xjsr305=strict")
             jvmTarget = "17"
         }
+//        sourceSets.main {
+//            withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+//                kotlin.srcDir("$buildDir/generated/source/kapt/main")
+//            }
+//        }
     }
 
     tasks.withType<Test> {
@@ -67,17 +76,6 @@ subprojects{
 project(":multimodule-api") {
     dependencies {
         implementation(project(":multimodule-core"))
-        // 쿼리 dsl
-        implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta") // 스프링부트 3.0 이상에서는 이렇게 변경 해줘야함.
-        kapt(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
-        sourceSets.main {
-            withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
-                kotlin.srcDir("$buildDir/generated/source/kapt/main")
-            }
-        }
-    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
-    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
-    }
 }
 
 //core 설정
@@ -85,6 +83,30 @@ project(":multimodule-core") {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         runtimeOnly("com.mysql:mysql-connector-j")
+
+        // 쿼리 dsl
+//        implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta") // 스프링부트 3.0 이상에서는 이렇게 변경 해줘야함.
+//        kapt(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
+//        sourceSets.main {
+//            withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+//                kotlin.srcDir("$buildDir/generated/source/kapt/main")
+//            }
+//        }
+//        annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+//        annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
+
+        // 쿼리 dsl
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.0")
+        implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
+        implementation("com.infobip:infobip-spring-data-jpa-querydsl-boot-starter:8.1.1")
+        kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+     }
+
+    sourceSets.main {
+        withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+            kotlin.srcDir("$buildDir/generated/source/kapt/main")
+        }
+    }
 
     }
 
--- multimodule-api/build.gradle.kts
+++ multimodule-api/build.gradle.kts
@@ -4,9 +4,10 @@ plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
+    kotlin("kapt") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
-    kotlin("kapt") version "1.8.21"
+    kotlin("plugin.allopen") version "1.8.21"
 
 }
 
